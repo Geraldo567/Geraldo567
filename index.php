50b40c2dd6c4cf3132e2c343685c87f3394ac666
Geraldo567 <linogeraldo955@gmail.com>
2025-03-02 23:38:30 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="_css/bootstrap.min.css" rel="stylesheet" >
+	<link href="_css/bootstrap.css" rel="stylesheet" >
+	<link href="_css/responsive.css" rel="stylesheet" >
+	<link rel="stylesheet" href="./_css/estilo.css">
+	<title></title>
+</head>
+<body>
+
+<header>
+	<h4>Luther</h4>
+	<h5>Eventos</h5>
+	<nav>
+		<ul>
+            <li><a href="index.php"style="color:white;">HOME</a></li>
+			<li><a href="evento.php">EVENTOS</a></li>
+			<li><a href="músicas.php">MÚSICA</a></li>
+			<li><a href="faleconnosco.php">CONTACTO</a></li>
+			<li><a href="sobrenos.php">SOBRE NÓS</a></li>
+		</ul>
+	</nav>
+</header>
+
+<main>
+<section class="release">
+   <img src="_imagens/Captura de Ecrã (72).jpg">
+</section>
+
+<section class="releas">
+	<p><h3>Sejá bem-vindo a plataforma Luther Eventos</h3>
+aqui encontras as novidades sobre os teus artistas suas músicas e eventos<br>
+fique ligado em nós.</p>
+</section>
+
+</main>
+
+<footer>
+	<div class="footer">
+    <p><h3>Luther Eventos</h3>&copy; 2024 Todos os direitos reservados.</p>
+	</div>
+</footer>
+</body>
+</html>
